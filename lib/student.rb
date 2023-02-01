f2430ab9a8e7a42fec44570a8e79438771a1ca4d
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2023-02-01 15:32:20 -0500

    commit tree e66d2eb917bb79b687addac0a2b8daef2c7459d3

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+    attr_reader :knowledge
+
+    def initialize
+        @kowledge = []
+    end
+
+    def learn (knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        return knowledge
+    end
+
+    #??Define a method, `#knowledge`, that returns that student's knowledge array.
 
 end
